35f36adf84aa270aa87a53b313b08b1761902d74
Adrián Ortuño <adrian.ortuno@sumamoos.com>
2019-09-10 17:32:41 +0200

    Subidas las librerías del proyecto
    Subidas las librerías del proyecto

---
+++ vendor/madebyais/facebook-go-sdk/examples/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// Run generate access token
+	GenerateAccessToken()
+
+	// Run basic sample for me
+	BasicMe()
+
+	// Run basic sample for feed
+	BasicFeed()
+
+	// Run basic sample for how to POST a feed
+	BasicFeedPost()
+
+	// Run basic sample for how to DELETE a feed
+	BasicFeedDelete()
+}
